dd3e125c39aa89e58d71644244d7290ae65ad83d
Rodrigo Almeida <br6rodrigo@gmail.com>
2022-01-27 15:41:38 -0300

    Projeto TEXO

--- src/test/java/test/demoQASelenium.java
+++ src/test/java/test/demoQASelenium.java
@@ -1,7 +1,7 @@
 package test;
 
 import com.google.gson.GsonBuilder;
-import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -11,7 +11,7 @@ import pojo.Album;
 
 import java.util.Arrays;
 import java.util.List;
-
+import java.util.Optional;
 
 
 public class demoQASelenium {
@@ -28,11 +28,13 @@ public class demoQASelenium {
         browser.findElement(By.xpath("/html/body/div/main/ul/li[2]/a")).click();
         String aux = browser.findElement(By.xpath("//html")).getText();
         List<Album> list = Arrays.asList(new GsonBuilder().create().fromJson(aux, Album[].class));
-        Album selecionado = list.stream().filter(item -> item.getId().equals("6")).findFirst().get();
-        Assert.assertEquals("1", selecionado.getAlbumId());
-        Assert.assertEquals("accusamus ea aliquid et amet sequi nemo", selecionado.getTitle());
-        Assert.assertEquals("https://via.placeholder.com/600/56a8c2", selecionado.getUrl());
-        Assert.assertEquals("https://via.placeholder.com/150/56a8c2", selecionado.getThumbnailUrl());
+        Optional<Album> selecionado = list.stream().filter(item -> item.getId().equals("6")).findFirst();
+        if (selecionado.isPresent()) {
+            Assertions.assertEquals("1", selecionado.get().getAlbumId());
+            Assertions.assertEquals("accusamus ea aliquid et amet sequi nemo", selecionado.get().getTitle());
+            Assertions.assertEquals("https://via.placeholder.com/600/56a8c2", selecionado.get().getUrl());
+            Assertions.assertEquals("https://via.placeholder.com/150/56a8c2", selecionado.get().getThumbnailUrl());
+        }
         browser.close();
 
     }
